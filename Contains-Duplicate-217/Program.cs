43c0adcc97fe9e2953bfbe962fca51c10a5889b3
gelisenyazilimci <burbytes@gmail.com>
2024-12-12 11:08:01 +0300

    217 Completed

---
+++ Contains-Duplicate-217/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+public class Solution {
+    public static bool ContainsDuplicate(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i].Equals(nums[j]))
+                {
+                    return true;
+                    break;
+                }
+            } 
+        }
+        return false; 
+    }
+}
