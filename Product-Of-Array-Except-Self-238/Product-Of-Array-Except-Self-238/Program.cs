607cad15ec71c18a7a86627c213c71d0a078981a
gelisenyazilimci <burbytes@gmail.com>
2025-01-29 12:44:41 +0300

    238 Completed

---
+++ Product-Of-Array-Except-Self-238/Product-Of-Array-Except-Self-238/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Product_Of_Array_Except_Self_238;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        int zeros = 0, idx = -1, prod = 1;
+        var n = nums.Length;
+        int[] newNums;
+        
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                zeros++;
+                idx = i;
+            } else {
+                prod *= nums[i];
+            }
+        }
+
+        newNums = new int[n];
+        Array.Fill(newNums, 0);
+
+        switch (zeros)
+        {
+            case 0:
+            {
+                for (int i = 0; i < n; i++) newNums[i] = prod / nums[i];
+                break;
+            }
+            case 1:
+                newNums[idx] = prod;
+                break;
+        }
+        return newNums;
+    }
+}
