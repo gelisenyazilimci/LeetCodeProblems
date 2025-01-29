c91199c5660f48f8bf61b8d3c70f3a89d011c189
gelisenyazilimci <burbytes@gmail.com>
2025-01-29 11:43:30 +0300

    151 Completed

---
+++ Reverse-Words-In-A-String-151/Reverse-Words-In-A-String-151/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Reverse_Words_In_A_String_151;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    public string ReverseWords(string s)
+    {
+        s = s.Trim();
+        string[] words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        Array.Reverse(words);
+        return string.Join(" ", words);
+    }
+}
