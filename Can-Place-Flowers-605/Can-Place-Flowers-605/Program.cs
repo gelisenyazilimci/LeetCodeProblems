fa4444aef1254cfa0b36cf39dc40920976d69e70
gelisenyazilimci <burbytes@gmail.com>
2025-01-29 08:40:19 +0300

    605 Completed

---
+++ Can-Place-Flowers-605/Can-Place-Flowers-605/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Can_Place_Flowers_605;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine(CanPlaceFlowers([1,0,0,0,0,0,1], 2));
+    }
+    
+    public static bool CanPlaceFlowers(int[] flowerbed, int n)
+    {
+        int count = 0;
+        
+        for (int i = 0; i < flowerbed.Length; i++)
+        {
+            if (flowerbed[i] == 0)
+            {
+                bool emptyPrev = (i == 0) || (flowerbed[i - 1] == 0);
+                bool emptyNext = (i == flowerbed.Length - 1) || (flowerbed[i + 1] == 0);
+
+                if (emptyPrev && emptyNext)
+                {
+                    flowerbed[i] = 1; 
+                    count++;
+                    if (count >= n) return true;
+                }
+            }
+        }
+
+        return count >= n;
+    }
+    
+}
