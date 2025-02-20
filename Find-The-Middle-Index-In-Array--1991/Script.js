088ea0d5a2ab27f27caac6d659caf4f9c24ed222
gelisenyazilimci <burbytes@gmail.com>
2025-02-20 12:06:50 +0300

    1991 Completed

---
+++ Find-The-Middle-Index-In-Array--1991/Script.js
@@ -0,0 +1,18 @@
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+let findMiddleIndex = function(nums) {
+    let leftSum = 0, rightSum = nums.reduce((a , b) => a + b);
+    for (let i = 0; i < nums.length; ++i){
+        rightSum -= nums[i];
+        if (rightSum === leftSum) return i;
+        leftSum += nums[i];
+    }
+    return -1;
+}
