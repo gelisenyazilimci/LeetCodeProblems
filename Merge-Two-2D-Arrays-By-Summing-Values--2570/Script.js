ad7e0df6e504fc31c07091611c1d22ed0928e4a6
gelisenyazilimci <burbytes@gmail.com>
2025-03-02 10:16:38 +0300

    2570 Fix

--- Merge-Two-2D-Arrays-By-Summing-Values--2570/Script.js
+++ Merge-Two-2D-Arrays-By-Summing-Values--2570/Script.js
@@ -14,11 +14,9 @@ const mergeArrays = function(nums1, nums2) {
         if (id1 === id2) { result.push([id1, val1 + val2]); o++; t++;
         } else if (id1 < id2) { result.push([id1, val1]); o++; }
         else { result.push([id2, val2]); t++; }
-        id1 === id2 ? result.push([id1, val1 + val2])
     }
     for (;o < nums1.length; o++) result.push(nums1[o]);
     for (;t < nums2.length; t++) result.push(nums2[t]);
     return result;
 }
-
 console.log(mergeArrays([[2,4],[3,6],[5,5]],[[1,3],[4,3]]));
