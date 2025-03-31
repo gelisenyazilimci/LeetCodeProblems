53c8386f95cca90547602f393297caf2c6bc2856
gelisenyazilimci <burbytes@gmail.com>
2025-03-31 23:36:41 +0300

    3394 Completed

---
+++ Put-Marbles-In-Bags--2551/Script.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} weights
+ * @param {number} k
+ * @return {number}
+ */
+let putMarbles = function (weights, k) {
+    if (k === 1) return 0;
+    let pairSums = [], minScore = 0, maxScore = 0;
+    for (let i = 0; i < weights.length - 1; i++) pairSums.push(weights[i] + weights[i + 1]);
+    pairSums.sort((a, b) => a - b);
+    for (let i = 0; i < k - 1; i++) {
+        minScore += pairSums[i]; maxScore += pairSums[weights.length - 2 - i];
+    }
+    return maxScore - minScore;
+}
